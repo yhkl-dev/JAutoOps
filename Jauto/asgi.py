a7bf7ca821ca09e85e5e3946e5efa206f7745fe6
yhkl-dev <kaiyang939325@gmail.com>
2021-08-19 23:11:52 +0800

    update

---
+++ Jauto/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Jauto project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jauto.settings')
+
+application = get_asgi_application()
---
+++ apps/users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    name = models.CharField("NAME", max_length=32, null=True, help_text="NAME")
+    phone = models.CharField("PHONE", max_length=11, null=True, help_text="PHONE")
+    github_username = models.CharField("GITHUB ACCOUNT", max_length=11, null=True, help_text="GITHUB ACCOUNT")
+    github_token = models.CharField("GITHUB TOKEN", max_length=100, null=True, help_text="GITHUB TOKEN")
+    id_rsa_key = models.TextField(null=True)
+    id_rsa_pub = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = "用户"
+        ordering = ["id"]
+        db_table = 'auth_user'
+        permissions = (
+            ("can_view_user", "cat view user"),
+        )
+
+    def __str__(self):
+        return self.username
+
+    # def get_view_permissions(self):
+    #     if self.is_superuser:
+    #         return Menu.objects.all()
+    #     return Menu.objects.filter(groups__in=self.groups.all())
